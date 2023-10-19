a834d861860d0e1bc023a4fbfbc2aa657aa79b61
Shobhit goyal <armyshobhit67@gmail.com>
2023-10-19 20:52:55 +0530

    First commit

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -21,9 +21,7 @@ function App() {
         <Route path="/about" element={<About/>} />
         <Route path="/services" element={<Services/>} />
         <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        {/* <Redirect to="/" /> */}
-        
+        <Route path="/register" element={<Register/>} />        
       </Routes>
       <Footer />
     </div>
