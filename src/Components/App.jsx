d3670278ade71682d6378099d70bd1f443dcaaaf
Shobhit goyal <armyshobhit67@gmail.com>
2023-10-19 22:11:33 +0530

    Home page responsive

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
